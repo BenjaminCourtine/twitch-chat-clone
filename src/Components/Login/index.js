b587a7accd93cedc9461af62be727dabd5ed9044
Benjamin Courtine <benjamin.courtine@supinternet.com>
2019-11-16 17:53:42 +0100

    Login completed

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pseudo: '',
+    }
+  }
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.state.pseudo.length < 3 ?
+      document.querySelector('.err-message').innerHTML = 'Your pseudo is to short'
+    :
+      this.props.history.push(`/pseudo/${this.pseudo.value}`)
+  }
+
+  handleChange = (e) => {
+    this.setState({pseudo: e.target.value})
+  }
+
   render() {
     return(
-      <div>Login</div>
+      <form className="login-form" onSubmit={(e) => this.handleSubmit(e)}>
+        <input type="text"
+               placeholder="Pseudo"
+               ref={input => this.pseudo = input}
+               value={this.state.pseudo}
+               onChange={e => this.handleChange(e)}
+        />
+        <span className="err-message"></span>
+        <button type="submit">Login</button>
+      </form>
     )
   }
 }
